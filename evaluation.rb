691bd2fb46f0b9d9d66627aca395cf5e9a64bab6
julienemo <juliette.nada@gmail.com>
2022-06-25 01:10:37 +0200

    Cleans evaluation checking conditions

--- evaluation.rb
+++ evaluation.rb
@@ -1,6 +1,7 @@
+require 'pry'
 class Evaluation
   TYPES = { siren: 'SIREN', vat: 'VAT' }.freeze
-  
+
   attr_reader :type, :value, :score, :state, :reason
 
   def initialize(type:, value:, score:, state:, reason:)
@@ -19,5 +20,12 @@ class Evaluation
     @state = state || @state
     @reason = reason || @reason
     @score = score || @score
+  end
+
+  def should_be_evalutated?
+    return false if @score.zero? && @state == 'unfavorable'
+
+    unconfirmed_for_ongoing_update = @state == 'unconfirmed' && reason == 'ongoing_database_update'
+    unconfirmed_for_ongoing_update || @score.zero?
   end
 end
--- types/siren.rb
+++ types/siren.rb
@@ -7,10 +7,10 @@ class Siren
   end
 
   def update
-    if @evaluation.score > 0 && @evaluation.state == "unconfirmed" && @evaluation.reason == "ongoing_database_update"
-      company_state =::OpenDataService::get_company_state(@evaluation.value)
+    if @evaluation.should_be_evalutated?
+      company_state = ::OpenDataService.get_company_state(@evaluation.value)
       return @evaluation.assign_fields(state: "favorable", reason: "company_opened", score: 100) if company_state == "Actif"
-        
+
       @evaluation.assign_fields(state: "unfavorable", reason:"company_closed", score: 100)
     elsif @evaluation.score >= 50
       if @evaluation.state == "unconfirmed" && @evaluation.reason == "unable_to_reach_api"
@@ -24,7 +24,7 @@ class Siren
       end
     else
       if @evaluation.state == "favorable" || @evaluation.state == "unconfirmed"
-        company_state =::OpenDataService::get_company_state(@evaluation.value)
+        company_state = ::OpenDataService.get_company_state(@evaluation.value)
         return @evaluation.assign_fields(state: "favorable", reason: "company_opened", score: 100) if company_state == "Actif"
 
         @evaluation.assign_fields(state: "unfavorable", reason: "company_closed", score: 100)
