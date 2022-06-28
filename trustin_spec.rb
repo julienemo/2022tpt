ee25bd5dec3d6224d5f5be595ebdf9d7b902bcbb
julienemo <juliette.nada@gmail.com>
2022-06-28 22:14:32 +0200

    Stops sending real API call in specs

--- trustin_spec.rb
+++ trustin_spec.rb
@@ -6,12 +6,13 @@ require_relative './services/fake_vat_service'
 RSpec.describe TrustIn do
   describe '.update_score_for_all' do
     context 'when evaluation type is "SIREN"' do
-      subject! { described_class.update_score_for_all(evaluations) }
+      subject { described_class.update_score_for_all(evaluations) }
 
       context "with a <score> greater or equal to 50 AND the <state> is unconfirmed and the <reason> is 'unable_to_reach_api'" do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "123456789", score: 79, state: "unconfirmed", reason: "unable_to_reach_api")] }
 
         it "decreases the <score> of 5" do
+          subject
           expect(evaluations.first.score).to eq(74)
         end
       end
@@ -20,6 +21,7 @@ RSpec.describe TrustIn do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "123456789", score: 37, state: "unconfirmed", reason: "unable_to_reach_api")] }
 
         it "decreases the <score> of 1" do
+          subject
           expect(evaluations.first.score).to eq(36)
         end
       end
@@ -28,6 +30,7 @@ RSpec.describe TrustIn do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "123456789", score: 28, state: "favorable", reason: "company_opened")] }
 
         it "decreases the <score> of 1" do
+          subject
           expect(evaluations.first.score).to eq(27)
         end
       end
@@ -35,7 +38,10 @@ RSpec.describe TrustIn do
       context "when the <state> is 'unconfirmed' AND the <reason> is 'ongoing_database_update'" do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "832940670", score: 42, state: "unconfirmed", reason: "ongoing_database_update")] }
 
+        before { allow(OpenDataService).to receive(:get_company_state).and_return('Actif')}
+
         it "assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100" do
+          subject
           expect(evaluations.first.state).to eq("favorable")
           expect(evaluations.first.reason).to eq("company_opened")
           expect(evaluations.first.score).to eq(100)
@@ -45,7 +51,10 @@ RSpec.describe TrustIn do
       context "with a <score> equal to 0" do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "320878499", score: 0, state: "favorable", reason: "company_opened")] }
 
+        before { allow(OpenDataService).to receive(:get_company_state).and_return('Inactif')}
+
         it "assigns a <state> and a <reason> to the evaluation based on the API response and a <score> to 100" do
+          subject
           expect(evaluations.first.state).to eq("unfavorable")
           expect(evaluations.first.reason).to eq("company_closed")
           expect(evaluations.first.score).to eq(100)
@@ -64,6 +73,7 @@ RSpec.describe TrustIn do
         let(:evaluations) { [Evaluation.new(type: "SIREN", value: "123456789", score: 0, state: "unfavorable", reason: "company_closed")] }
 
         it "does not call the API" do
+          subject
           expect(Net::HTTP).not_to receive(:get)
         end
       end
